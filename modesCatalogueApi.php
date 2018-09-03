6aa883e3a25837b889f40dda81ea6f2a4c891bf0
mvl22 <martin@lucas-smith.co.uk>
2018-09-03 22:02:14 +0100

    Commit implementation-specific fixes temporarily in place

--- modesCatalogueApi.php
+++ modesCatalogueApi.php
@@ -466,6 +466,7 @@ class modesCatalogueApi extends frontControllerApplication
 		$this->databaseConnection->delete ($this->settings['database'], 'collections', $constraints);
 		
 		# Add collections-level entries for this grouping into the collections table
+		#!# Implementation-specific fixes for sponsorNotice, imagesSubfolder need to be generalised
 		#!# Status=R: There are 6 different /Administration/Progress/Keyword currently A,B,H,P,R,Y - we currently use R and P; the others need to be documented; Collections are aware of this as of 180524
 		$query = "INSERT INTO {$this->settings['database']}.collections
 			(
@@ -483,7 +484,7 @@ class modesCatalogueApi extends frontControllerApplication
 					NULL AS aboutPageTabText,
 					NULL AS contactsPageHtml,
 					NULL AS contactsPageEmail,
-					NULL AS sponsorNotice,
+					IF(id = 'ANTC', '<p><img src=\"/museum/catalogue/antc/sponsor.jpg\" width=\"250\" alt=\"\" border=\"0\" /></p>', NULL) AS sponsorNotice,
 					NULL AS categoriesTable,
 					1 AS disableCategories,
 					1 AS disableMaterials,
