f96130d44d60ced5bea29caf3012b7341c0eaee4
mvl22 <martin@lucas-smith.co.uk>
2025-05-05 20:22:54 +0100

    Fix undefined tick icon

--- import.php
+++ import.php
@@ -111,7 +111,7 @@ class import
 		
 		# Confirm the result
 		if ($recordsDone) {
-			$html .= "\n<p>{$this->tick} <strong>Success: " . number_format ($recordsDone) . ' records refreshed / imported into the database.</strong></p>';
+			$html .= "\n<p>" . '<img src="/images/icons/tick.png" alt="Tick" class="icon" /> <strong>Success: ' . number_format ($recordsDone) . ' records refreshed / imported into the database.</strong></p>';
 			$html .= "\n<p>Max memory used: " . round (memory_get_peak_usage (true) / 1048576, 2) . 'MB.</p>';
 			$html .= "\n<p><a href=\"{$this->baseUrl}/import/\">Reset this page.</a></p>";
 		}
